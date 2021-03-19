6023eb8290dc69726af9ee4551588494969b9944
EC2 Default User <ec2-user@ip-10-0-0-223.ec2.internal>
2021-03-19 20:51:22 +0000

    ya casi login

--- src/client.js
+++ src/client.js
@@ -6,8 +6,9 @@ const rl = require('readline').createInterface({
 	output: process.stdout
 });
 var AuthUser="";
-
-
+var cont=1;
+var state= "";
+var user_id=0;
 const options = {
   hostname: 'ec2-52-4-113-182.compute-1.amazonaws.com',
   port: 3000,
@@ -18,67 +19,62 @@ const options = {
    //, 'Content-Length': data.length
   }
 }
-function auth(){
 
-rl.question('login or register ', answer => {
-  // TODO: Log the answer in a database
 
-var args2 = answer.trim().replace(/  +/g, ' ');
-args2 = args2.split(/\s+/); //Split by space
-
-if (args2.length >= 1 ){
-        switch (args2[0]) {
-                case "login":
-                        rl.question('enter username and password ', answer => {
-                          // TODO: Log the answer in a database
-                        var args2 = answer.trim().replace(/  +/g, ' ');
-                        args2 = args2.split(/\s+/); //Split by space
-                        var username = args2[0];
-                        var password = args2[1];
-                        console.log(username + password);
-                        var user= false;
-                        fs.readFile("auth.txt", 'utf8', (error, datos) => {
-                         if (error) throw error;
-                        datos = datos.split(/\n/g);
-                        var i = 0;
-                        while(user == false && i< datos.length){
-                                console.log(i);
-                                console.log(datos.length);
-                                 args2 = datos[i].split(/\s+/);
-                                if(username == args2[0] && password== args2[1])
-                                user = true;
-                        i++;
-                        }
-                        if(user == true){
-                        AuthUser= username
-                        }
-                        console.log(AuthUser);
-
-                        });
-                        });
-                        break;
-                case "register":
-                        console.log("register 2 ");
-                        break;
-                default :
-                        break;
-}
-}
-});
-}
 var recursiveAsyncReadLine = function () {
-
-
-rl.question('What is the message? ', message => {
-
+rl.question('What do you want to do?', message => {
+var valid = true;
 if (message == 'exit')
-	return readline.close();
+	return rl.close();
 
 var args = message.trim().replace(/  +/g, ' ');
 args = args.split(/\s+/); //Split by space
-
-
-if (args.length >= 2 ){
+console.log("Esto es args - ");
+console.log(args);
+if(cont = 1 && (args[0] == "login" ||args[0] == "register")){
+	console.log("El argumento es login o register, nice");
+        state = (args[0]== "login")?"login":"register"; //Esto funcionará? XD Es un if then else.
+	console.log("Please type username and then password");
+	cont++;
+}
+else if (cont ==2){
+	console.log("Contador es 2.");
+	cont++;
+	var username = args[0];
+	var password = args[1];
+	var user= false;
+	switch (state) {
+		case "login":
+		fs.readFile("/home/ec2-user/Proyecto1/src/auth.txt", 'utf8', (error, datos) => {
+                if (error) throw error;
+                datos = datos.split(/\n/g);
+                var i = 0;
+                while(user == false && i< datos.length){
+                	console.log(i);
+                        console.log(datos.length);
+                        args2 = datos[i].split(/\s+/);
+                        if(username == args2[0] && password== args2[1])
+                        	user = true;
+				user_id= i;
+                        	i++;
+                        }
+                        if(user == true){
+                        	AuthUser= username;
+                        }
+                        console.log(AuthUser);
+			console.log("Entra a login ");
+                        });
+			break;
+		case "register":
+			console.log("register");
+			break;
+		default:
+			console.log("Si entra aquí, la cagué");
+			break;
+	}
+}
+else {
+if (args.length >= 2 && cont>2 ){
 	var to_send= "";
 	switch (args[0]) {
 		case "pull":
@@ -106,40 +102,43 @@ if (args.length >= 2 ){
 			break;
 
 		default:
+			valid=false;
 			console.log("None of the methods were valid");
+			break;
 			//recursiveAsyncReadLine(); //Does nothing and writes error, no estoy seguro si esto debería hacerlo así porque volvería a llamar la función , hmm, veremos.
 
 }
+if (valid){
+	var data = JSON.stringify({
+        	to_send
+	});
 
- // If it enters the switch, then it will do this, right?
-var data = JSON.stringify({
-        to_send
-});
 
+	const req = https.request(options, res => {
+	console.log(`statusCode: ${res.statusCode}`)
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
+  	res.on('data', d => {
+    	  process.stdout.write(d)
+  	  })
+	})
 
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
+	req.on('error', error => {
+  	  console.error(error)
+	})
 
-req.on('error', error => {
-  console.error(error)
-})
-
-req.write(data);
-req.end();
-
+	req.write(data);
+	req.end();
+	}
 }
 else {
-	console.log("Expected 2 arguments");
+	console.log("Expected 2 arguments or error in typo");
+	}
 }
 recursiveAsyncReadLine();
 
 	});
 };
-auth();
+
+
 recursiveAsyncReadLine();
 
