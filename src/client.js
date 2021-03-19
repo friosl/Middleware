e77f9ebd29db3743cf43c33e8e4828ba3d71a43f
EC2 Default User <ec2-user@ip-10-0-0-223.ec2.internal>
2021-03-19 04:16:32 +0000

    auth a medias

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 const https = require('http')
-
+const fs = require("fs")
 
 const rl = require('readline').createInterface({
 	input: process.stdin,
@@ -19,8 +19,47 @@ const options = {
   }
 }
 
+rl.question('login or register ', answer => {
+  // TODO: Log the answer in a database
 
+var args2 = answer.trim().replace(/  +/g, ' ');
+args2 = args2.split(/\s+/); //Split by space
+
+if (args2.length >= 1 ){
+        switch (args2[0]) {
+                case "login":
+			rl.question('enter username and password ', answer => {
+			  // TODO: Log the answer in a database
+
+		
+			var args2 = answer.trim().replace(/  +/g, ' ');
+			args2 = args2.split(/\s+/); //Split by space
+			var username = args2[0];
+			var password = args2[1];
+			console.log(username + password);
+			var user= false;
+ 			fs.readFile("auth.txt", 'utf8', (error, datos) => {
+   			 if (error) throw error;
+			datos = datos.split(/\n/g);
+			for(var i = 0;i<datos.length; i++){
+                                args2 = datos[i].split(/\s+/);
+				if(username == args2[0] && password[1])
+				user = true;
+                        }
+			console.log(user);
+			});
+			});
+                        break;
+                case "register":
+                        console.log("register 2 ");
+                        break;
+                default :
+                        break;
+}
+}
+});
 var recursiveAsyncReadLine = function () {
+
 
 rl.question('What is the message? ', message => {
 
